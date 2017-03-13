670bb04768b7a34c93648dc3544066ec6ab97791
Akshit(AK) Gupta <akg009@ucsd.edu>
2017-03-12 18:03:38 -0700

    post and put http

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+app.use(bodyParser.json());
+
 Genre = require('./models/genre');
 Book = require('./models/book');
 
@@ -23,12 +25,63 @@ app.get('/api/genres', function(req,res){
 	})
 });
 
+app.post('/api/genres', function(req,res){
+	var genre = req.body;
+	Genre.addGenres(genre, function(err, genre){
+		if(err){
+			throw err;
+		}
+		res.json(genre);
+	})
+});
+
+app.put('/api/genres/:_id', function(req,res){
+	var id = req.params._id;
+	genre = req.body;
+	Genre.updateGenre(id, genre, {}, function(err, genre){
+		if(err){
+			throw err;
+		}
+		res.json(genre);
+	})
+});
+
 app.get('/api/books', function(req,res){
 	Book.getBooks(function(err, books){
 		if(err){
 			throw err;
 		}
 		res.json(books);
+	})
+});
+
+app.get('/api/books/:_id', function(req,res){
+	Book.getBookById(req.params._id, function(err, book){
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	})
+});
+
+app.post('/api/books', function(req,res){
+	var book = req.body;
+	Book.addBook(book, function(err, book){
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	})
+});
+
+app.put('/api/books/:_id', function(req,res){
+	var id = req.params._id;
+	book = req.body;
+	Book.updateBook(id, book, {}, function(err, book){
+		if(err){
+			throw err;
+		}
+		res.json(book);
 	})
 });
 
--- models/genre.js
+++ models/genre.js
@@ -18,3 +18,18 @@ var Genre = module.exports = mongoose.model('Genre', genreSchema);
 module.exports.getGenres = function(callback, limit){
 	Genre.find(callback).limit(limit);
 }
+
+// Add Genres
+module.exports.addGenres = function(genre, callback){
+	Genre.create(genre, callback);
+}
+
+// Update Genres
+module.exports.updateGenre = function(id, genre, options, callback){
+	var query = {_id: id};
+	var update = {
+		name: genre.name
+	};
+
+	Genre.findOneAndUpdate(query, update, options, callback);
+}
